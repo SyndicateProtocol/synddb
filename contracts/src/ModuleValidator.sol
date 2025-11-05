adc3a7b34148977175a77c8e40ba84cfd7ebe45d
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2025-11-05 21:14:25 +0100

    fix: refactor ModuleValidator

--- contracts/src/ModuleValidator.sol
+++ contracts/src/ModuleValidator.sol
@@ -6,7 +6,7 @@ import {IBridge} from "./interfaces/IBridge.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-abstract contract ModuleValidator is Ownable {
+abstract contract ModuleValidator is Ownable, IBridge {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     EnumerableSet.AddressSet private preModules;
@@ -20,49 +20,49 @@ abstract contract ModuleValidator is Ownable {
     error InvalidModuleAddress();
     error ModuleAlreadyExists();
     error ModuleDoesNotExist();
-    error InvalidPreExecutionStage(IBridge.ProcessingStage stage);
-    error InvalidPostExecutionStage(IBridge.ProcessingStage stage);
-    error ModuleCheckFailed(address module, IBridge.ProcessingStage stage);
+    error InvalidPreExecutionStage(ProcessingStage stage);
+    error InvalidPostExecutionStage(ProcessingStage stage);
+    error ModuleCheckFailed(address module, ProcessingStage stage);
 
     constructor() Ownable(msg.sender) {}
 
-    function addPreModule(address module) external onlyOwner {
+    function addPreModule(address module) external virtual onlyOwner {
         if (module == address(0)) revert InvalidModuleAddress();
         if (!preModules.add(module)) revert ModuleAlreadyExists();
         emit PreModuleAdded(module);
     }
 
-    function addPostModule(address module) external onlyOwner {
+    function addPostModule(address module) external virtual onlyOwner {
         if (module == address(0)) revert InvalidModuleAddress();
         if (!postModules.add(module)) revert ModuleAlreadyExists();
         emit PostModuleAdded(module);
     }
 
-    function removePreModule(address module) external onlyOwner {
+    function removePreModule(address module) external virtual onlyOwner {
         if (module == address(0)) revert InvalidModuleAddress();
         if (!preModules.remove(module)) revert ModuleDoesNotExist();
         emit PreModuleRemoved(module);
     }
 
-    function removePostModule(address module) external onlyOwner {
+    function removePostModule(address module) external virtual onlyOwner {
         if (module == address(0)) revert InvalidModuleAddress();
         if (!postModules.remove(module)) revert ModuleDoesNotExist();
         emit PostModuleRemoved(module);
     }
 
-    function getPreModules() external view returns (address[] memory) {
+    function getPreModules() external view virtual returns (address[] memory) {
         return preModules.values();
     }
 
-    function getPostModules() external view returns (address[] memory) {
+    function getPostModules() external view virtual returns (address[] memory) {
         return postModules.values();
     }
 
     function _validateModules(
         EnumerableSet.AddressSet storage modules,
-        IBridge.ProcessingStage stage,
-        bytes calldata payload,
-        IBridge.ValidatorSignatures calldata executionSigs
+        ProcessingStage stage,
+        bytes memory payload,
+        ValidatorSignatures memory executionSigs
     ) internal returns (bool) {
         uint256 length = modules.length();
         for (uint256 i = 0; i < length; i++) {
@@ -74,23 +74,21 @@ abstract contract ModuleValidator is Ownable {
         return true;
     }
 
-    function _validatePreModules(
-        IBridge.ProcessingStage stage,
-        bytes calldata payload,
-        IBridge.ValidatorSignatures calldata executionSigs
-    ) internal returns (bool) {
-        if (stage != IBridge.ProcessingStage.PreExecution) {
+    function _validatePreModules(ProcessingStage stage, bytes memory payload, ValidatorSignatures memory executionSigs)
+        internal
+        returns (bool)
+    {
+        if (stage != ProcessingStage.PreExecution) {
             revert InvalidPreExecutionStage(stage);
         }
         return _validateModules(preModules, stage, payload, executionSigs);
     }
 
-    function _validatePostModules(
-        IBridge.ProcessingStage stage,
-        bytes calldata payload,
-        IBridge.ValidatorSignatures calldata executionSigs
-    ) internal returns (bool) {
-        if (stage != IBridge.ProcessingStage.PostExecution) {
+    function _validatePostModules(ProcessingStage stage, bytes memory payload, ValidatorSignatures memory executionSigs)
+        internal
+        returns (bool)
+    {
+        if (stage != ProcessingStage.PostExecution) {
             revert InvalidPostExecutionStage(stage);
         }
         return _validateModules(postModules, stage, payload, executionSigs);
