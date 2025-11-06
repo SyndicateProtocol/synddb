7badc7134e0ca0271afe42f2be5f9e14949455d0
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2025-11-06 12:45:46 +0100

    feat: add new module ERC20MaxSupplyIncreaseModule

---
+++ contracts/src/modules/ERC20MaxSupplyIncreaseModule.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {IModuleCheck} from "src/interfaces/IModuleCheck.sol";
+import {ProcessingStage, SequencerSignature} from "src/types/DataTypes.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ERC20MaxSupplyIncreaseModule is IModuleCheck {
+    IERC20 public immutable TOKEN;
+    uint256 public immutable MAX_SUPPLY_INCREASE;
+
+    uint256 public preExecutionSupply;
+
+    constructor(address _token, uint256 _maxSupplyIncrease) {
+        TOKEN = IERC20(_token);
+        MAX_SUPPLY_INCREASE = _maxSupplyIncrease;
+    }
+
+    function check(ProcessingStage stage, bytes memory, SequencerSignature memory) external returns (bool) {
+        uint256 currentSupply = TOKEN.totalSupply();
+
+        if (stage == ProcessingStage.PreExecution) {
+            preExecutionSupply = currentSupply;
+            return true;
+        } else if (stage == ProcessingStage.PostExecution) {
+            if (currentSupply > preExecutionSupply) {
+                uint256 supplyIncrease = currentSupply - preExecutionSupply;
+                if (supplyIncrease > MAX_SUPPLY_INCREASE) {
+                    return false;
+                }
+            }
+
+            preExecutionSupply = 0;
+            return true;
+        }
+
+        return true;
+    }
+}
--- contracts/src/modules/ERC20TotalSupplyCheckModule.sol
+++ contracts/src/modules/ERC20TotalSupplyCheckModule.sol
@@ -5,17 +5,17 @@ import {IModuleCheck} from "src/interfaces/IModuleCheck.sol";
 import {ProcessingStage, SequencerSignature} from "src/types/DataTypes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ERC20SupplyCheckModule is IModuleCheck {
+contract ERC20TotalSupplyCheckModule is IModuleCheck {
     IERC20 public immutable TOKEN;
-    uint256 public immutable MAX_SUPPLY;
+    uint256 public immutable TOTAL_SUPPLY_ALLOWED;
 
-    constructor(address _token, uint256 _maxSupply) {
+    constructor(address _token, uint256 _totalSupplyAllowed) {
         TOKEN = IERC20(_token);
-        MAX_SUPPLY = _maxSupply;
+        TOTAL_SUPPLY_ALLOWED = _totalSupplyAllowed;
     }
 
     function check(ProcessingStage, bytes memory, SequencerSignature memory) external view returns (bool) {
         uint256 currentSupply = TOKEN.totalSupply();
-        return currentSupply <= MAX_SUPPLY;
+        return currentSupply <= TOTAL_SUPPLY_ALLOWED;
     }
 }
