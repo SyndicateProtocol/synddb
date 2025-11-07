3a050b477858d6eb96a6d10a42c3348431b1ba88
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2025-11-07 11:27:50 +0100

    fix: add better error handling in ValidatorSignatureThresholdModule.sol

--- contracts/src/modules/ValidatorSignatureThresholdModule.sol
+++ contracts/src/modules/ValidatorSignatureThresholdModule.sol
@@ -11,13 +11,48 @@ contract ValidatorSignatureThresholdModule is IModuleCheck, Ownable {
     address[] public validators;
     uint256 public signatureThreshold;
 
+    error ZeroAddress();
+    error EmptyValidatorArray();
+    error DuplicateValidator(address validator);
+    error ThresholdZero();
+    error ThresholdExceedsValidatorCount(uint256 threshold, uint256 validatorCount);
+
     constructor(address bridge, address[] memory _validators, uint256 _signatureThreshold) Ownable(msg.sender) {
+        if (bridge == address(0)) {
+            revert ZeroAddress();
+        }
+        if (_validators.length == 0) {
+            revert EmptyValidatorArray();
+        }
+        if (_signatureThreshold == 0) {
+            revert ThresholdZero();
+        }
+        if (_signatureThreshold > _validators.length) {
+            revert ThresholdExceedsValidatorCount(_signatureThreshold, _validators.length);
+        }
+
+        // Check for duplicate validators
+        for (uint256 i = 0; i < _validators.length; i++) {
+            for (uint256 j = i + 1; j < _validators.length; j++) {
+                if (_validators[i] == _validators[j]) {
+                    revert DuplicateValidator(_validators[i]);
+                }
+            }
+        }
+
         BRIDGE = IValidatorSigningAndQuery(bridge);
         validators = _validators;
         signatureThreshold = _signatureThreshold;
     }
 
     function setThreshold(uint256 _signatureThreshold) external onlyOwner {
+        if (_signatureThreshold == 0) {
+            revert ThresholdZero();
+        }
+        if (_signatureThreshold > validators.length) {
+            revert ThresholdExceedsValidatorCount(_signatureThreshold, validators.length);
+        }
+
         signatureThreshold = _signatureThreshold;
     }
 
